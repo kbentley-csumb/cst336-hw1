1d34f4094b3a683922cde81b15abe700bebdca6f
Kevin Bentley <kbentley@csumb.edu>
2019-06-29 23:06:58 +0000

    Main page structure

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Kevin Bentley: Computer Science Topics</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header>
+          <h1>Computer Science Topics</h1>
+        </header>
+        <hr/>
+        
+        <nav>
+          <!--<a href="index.php" class="menuselected">Home</a><br>-->
+          <a href="gis.php">Geospatial Information Systems</a><br>
+          <p class="topicsummary">this is some information about GIS systems.</p>
+          <a href="embedded.php">Embedded Systems</a><br>
+          <p class="topicsummary">this is some information about Embedded systems.</p>
+          <a href="ci.php">Continuous Integration</a><br>
+          <p class="topicsummary">this is some information about CI systems.</p>
+        </nav>
+        
+<main>
+<br/>
+<h2>
+Click on one of the above links to learn about the topic.
+</h2>
+
+<br/><br/>
+</main>
+
+        <footer>
+            CST336 Internet Programming. <br/>
+            Background image from <a href="https://www.pexels.com/photo/technology-computer-lines-board-50711/">https://www.pexels.com/photo/technology-computer-lines-board-50711/</a><br>
+            2019&copy; Kevin Bentley<br/>
+            <strong>Disclaimer</strong> This information is only for academic purposes. The information herein, while accurate, may be incomplete.
+                <br/>
+                <br/>
+                <img src="img/otter-small.jpg" alt="Otter Logo" />
+        </footer>
+        
+    </body>
+
+</html>
